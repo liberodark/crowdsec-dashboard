91d6e3e0ebe8947a912d2bd658e762c798838669
Kerma Gérald <gandalf@gk2.net>
2021-07-23 13:49:19 +0200

    Update run.sh to add log, host and port
    Add log : /var/log/metabase.log
    Add host and port

--- run.sh
+++ run.sh
@@ -2,5 +2,7 @@
 
 TYPE="h2"
 DB="/opt/crowdsec/metabase.db"
+HOST="0.0.0.0"
+PORT="3000"
 
-MB_DB_TYPE="$TYPE" MB_DB_FILE="$DB" java -jar metabase.jar
+MB_JETTY_HOST="$HOST" MB_JETTY_PORT="$PORT" MB_DB_TYPE="$TYPE" MB_DB_FILE="$DB" java -DMB_JETTY_HOST="$HOST" -DMB_JETTY_PORT="$PORT" -jar metabase.jar >> /var/log/metabase.log
