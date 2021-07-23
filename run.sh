0aa4449e9e2145551358d56b9457fc3fa377e2cc
Kerma Gérald <gandalf@gk2.net>
2021-07-23 13:58:38 +0200

    Update run.sh
    modify for logging

--- run.sh
+++ run.sh
@@ -5,4 +5,7 @@ DB="/opt/crowdsec/metabase.db"
 HOST="0.0.0.0"
 PORT="3000"
 
+touch /var/log/metabase.log
+chown crowdsec:crowdsec /var/log/metabase.log
+
 MB_JETTY_HOST="$HOST" MB_JETTY_PORT="$PORT" MB_DB_TYPE="$TYPE" MB_DB_FILE="$DB" java -DMB_JETTY_HOST="$HOST" -DMB_JETTY_PORT="$PORT" -jar metabase.jar >> /var/log/metabase.log
