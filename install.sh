da0d3ddb8e4c1bc22f210f480059e342892ea02e
Kerma Gérald <gandalf@gk2.net>
2021-07-23 15:22:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,16 @@
 
 sudo mkdir -p /opt/crowdsec
 sudo cp run.sh /opt/crowdsec
-sudo cp ./*.db /opt/crowdsec
+#sudo cp ./*.db /opt/crowdsec
 sudo cp crowdsec-dashboard.service /etc/systemd/system/
 cd /opt/crowdsec || exit
-wget https://downloads.metabase.com/v0.38.0.1/metabase.jar
+sudo wget https://downloads.metabase.com/v0.40.1/metabase.jar
 # If you want to no use preinstalled DB
-#wget https://crowdsec-statics-assets.s3-eu-west-1.amazonaws.com/metabase_sqlite.zip
-#unzip metabase_sqlite.zip && rm -f metabase_sqlite.zip || exit
-#cp /opt/crowdsec/metabase.db/metabase.db.mv.db /opt/crowdsec/metabase.db.mv.db || exit
-#rm -rf metabase.db || exit
-#mv metabase.db.mv.db metabase.db || exit
+sudo wget https://crowdsec-statics-assets.s3-eu-west-1.amazonaws.com/metabase_sqlite.zip
+sudo unzip metabase_sqlite.zip && rm -f metabase_sqlite.zip || exit
+sudo cp /opt/crowdsec/metabase.db/metabase.db.mv.db /opt/crowdsec/metabase.db.mv.db || exit
+#sudo rm -rf /opt/crowdsec/metabase.db || exit
+#sudo rm -rf /opt/crowdsec/metabase.db.trace.db || exit
 sudo ln -s /var/lib/crowdsec/data/crowdsec.db /opt/crowdsec/crowdsec.db || exit
 sudo groupadd -r crowdsec
 sudo useradd -r -g crowdsec -d /opt/crowdsec -s /sbin/nologin crowdsec
